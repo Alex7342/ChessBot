e9276050b6a739df200a2617ff51bac141917c46
Alex7342 <alexborgovan@yahoo.com>
2025-02-11 15:43:08 +0200

    Implemented en passant move generation for white pieces

--- ChessBot/Board.cpp
+++ ChessBot/Board.cpp
@@ -155,19 +155,32 @@ void Board::addPawnMoves(std::vector<Move>& moves, Piece piece)
 				moves.push_back(Move(piece.getPosition(), Position(row - 2, column)));
 		}
 
-		// Capture black piece up-left
 		if (validPosition(row - 1, column - 1))
+		{
+			// Capture black piece up-left
 			if (this->board[row - 1][column - 1].getType() != Piece::Type::NONE && this->board[row - 1][column - 1].getType() != Piece::Type::KING)
 				if (this->board[row - 1][column - 1].getColor() != piece.getColor())
 					moves.push_back(Move(piece.getPosition(), Position(row - 1, column - 1)));
 
-		// Capture black piece up-right
+			// En passant
+			if (this->board[row - 1][column - 1].getType() == Piece::Type::NONE)
+				if (this->board[row][column - 1].getType() == Piece::Type::PAWN && this->board[row][column - 1].getColor() != piece.getColor())
+					moves.push_back(Move(piece.getPosition(), Position(row - 1, column - 1)));
+		}
+
+		
 		if (validPosition(row - 1, column + 1))
+		{
+			// Capture black piece up-right
 			if (this->board[row - 1][column + 1].getType() != Piece::Type::NONE && this->board[row - 1][column + 1].getType() != Piece::Type::KING)
 				if (this->board[row - 1][column + 1].getColor() != piece.getColor())
-				moves.push_back(Move(piece.getPosition(), Position(row - 1, column + 1)));
+					moves.push_back(Move(piece.getPosition(), Position(row - 1, column + 1)));
 
-		// TODO Implement en passant
+			// En passant
+			if (this->board[row - 1][column + 1].getType() == Piece::Type::NONE)
+				if (this->board[row][column + 1].getType() == Piece::Type::PAWN && this->board[row][column + 1].getColor() != piece.getColor())
+					moves.push_back(Move(piece.getPosition(), Position(row - 1, column + 1)));
+		}
 	}
 	else
 	{
