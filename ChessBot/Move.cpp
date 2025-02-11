d4cb7de60c383f29ce5d13788a56ce5e3a9d25ec
Alex7342 <alexborgovan@yahoo.com>
2025-02-11 20:10:47 +0200

    Added a new field in the move class that holds data about pawn promotion

--- ChessBot/Move.cpp
+++ ChessBot/Move.cpp
@@ -2,7 +2,7 @@
 
 Move::Move() : initialPosition(Position()), targetPosition(Position()) {}
 
-Move::Move(const Position initialPosition, const Position targetPosition) : initialPosition(initialPosition), targetPosition(targetPosition) {}
+Move::Move(const Position initialPosition, const Position targetPosition, const Piece::Type promotionType) : initialPosition(initialPosition), targetPosition(targetPosition), promotionType(promotionType) {}
 
 Position Move::getInitialPosition()
 {
@@ -12,6 +12,16 @@ Position Move::getInitialPosition()
 Position Move::getTargetPosition()
 {
 	return this->targetPosition;
+}
+
+Piece::Type Move::getPromotionType()
+{
+	return this->promotionType;
+}
+
+void Move::setPromotionType(const Piece::Type newPromotionType)
+{
+	this->promotionType = newPromotionType;
 }
 
 std::string Move::toString() const
